dc5d2e61b4b68572210a67fc84b5bd1d43df8a72
Syntrice <syntrice@protonmail.com>
2025-03-11 18:09:24 +0000

    feat: Create User entity
    Create a user entity with Id, Username and PasswordHash properties for
    starters.

---
+++ SyntriceEShop.Common/Models/UserModel/User.cs
@@ -0,0 +1,11 @@
+namespace SyntriceEShop.Common.Models.UserModel;
+
+/// <summary>
+/// Entity for application user
+/// </summary>
+public class User : IEntity
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
