620712f06a0b8d743707c3f9a32a15b3f7c6e48c
Syntrice <syntrice@protonmail.com>
2025-03-11 17:53:16 +0000

    feat: Add boilerplate WebApplication code

--- SyntriceEShop.API/Program.cs
+++ SyntriceEShop.API/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+await app.RunAsync();
+
