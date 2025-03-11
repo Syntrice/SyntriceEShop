6b65c86dc4029ad5dc17af51a8e652a475d90cfb
Syntrice <syntrice@protonmail.com>
2025-03-11 23:11:41 +0000

    feat: Scaffold login and register endpoints

---
+++ SyntriceEShop.API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SyntriceEShop.Common.Models.UserModel;
+
+namespace SyntriceEShop.API.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+
+    [HttpPost]
+    [Route("register")]
+    public Task<OkResult> RegisterAsync([FromBody] UserRegisterDTO userRegisterDTO)
+    {
+        return Task.FromResult(Ok());
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public Task<OkResult> LoginAsync([FromBody] UserLoginDTO userLoginDTO)
+    {
+        return Task.FromResult(Ok());
+    }
+}
