a47d80caaff3c7910b6b64ccabb8c5c7a2f6ae87
Syntrice <syntrice@protonmail.com>
2025-03-16 14:07:40 +0000

    feat: Implement useCookies option for login endpoint
    The login endpoint now takes a query parameter, useCookies. If set to
    true, a refreshToken and accessToken secure http only cookie will be
    returned instead of a response body with the same information.

--- SyntriceEShop.API/Controllers/IUserController.cs
+++ SyntriceEShop.API/Controllers/IUserController.cs
@@ -7,7 +7,7 @@ namespace SyntriceEShop.API.Controllers;
 public interface IUserController
 {
     Task<IActionResult> RegisterAsync([FromBody] UserRegisterRequestDTO userRegisterRequestDto);
-    Task<IActionResult> LoginAsync([FromBody] UserLoginRequestDTO userLoginRequestDto);
+    Task<IActionResult> LoginAsync([FromBody] UserLoginRequestDTO userLoginRequestDto, [FromQuery] bool useCookies = false);
 
     Task<IActionResult> RefreshAsync([FromBody] UserRefreshRequestDTO userRefreshRequestDto);
 }
