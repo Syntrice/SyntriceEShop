71c56275af4a54e3e31b7581929a1bbc195fa0f0
Syntrice <syntrice@protonmail.com>
2025-03-19 14:06:53 +0000

    feat: Create tests an d implement OrderController
    Implement OrderController with TDD

--- SyntriceEShop.API/Controllers/Implementations/OrderController.cs
+++ SyntriceEShop.API/Controllers/Implementations/OrderController.cs
@@ -1,36 +1,87 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using SyntriceEShop.API.Controllers.Interfaces;
 using SyntriceEShop.API.Models.OrderModel.DTO;
+using SyntriceEShop.API.Services;
+using SyntriceEShop.API.Services.Interfaces;
 
 namespace SyntriceEShop.API.Controllers.Implementations;
 
 [ApiController]
 [Route("api/order")]
-public class OrderController : ControllerBase, IOrderController
+public class OrderController(IOrderService orderService) : ControllerBase, IOrderController
 {
     [HttpGet]
     public async Task<IActionResult> GetAllOrdersAsync()
     {
-        return Ok();
+        var result = await orderService.GetAllOrdersAsync();
+        
+        if (result.Type == ServiceResponseType.Success)
+        {
+            return Ok(result.Value);
+        }
+        
+        return StatusCode(500);
     }
     
     [HttpGet]
     [Route("{id:int}")]
     public async Task<IActionResult> GetOrderByIdAsync([FromRoute] int id)
-    {
-        return Ok();
+    {   
+        var result = await orderService.GetOrderByIdAsync(id);
+
+        if (result.Type == ServiceResponseType.Success)
+        {
+            return Ok(result.Value);
+        }
+
+        if (result.Type == ServiceResponseType.NotFound)
+        {
+            return NotFound();
+        }
+        
+        return StatusCode(500);
     }
     
     [HttpPost]
     public async Task<IActionResult> AddOrderAsync([FromBody] AddOrderRequest addOrderRequest)
     {
-        return Ok();
+        var result = await orderService.AddOrderAsync(addOrderRequest);
+        
+        if (result.Type == ServiceResponseType.Success)
+        {
+            return Ok(result.Value);
+        }
+        
+        if (result.Type == ServiceResponseType.Conflict)
+        {
+            return Conflict(result.Message);
+        }
+        
+        if (result.Type == ServiceResponseType.ValidationError)
+        {
+            return BadRequest(result.Message);
+        }
+        
+        return StatusCode(500);
     }
     
     [HttpDelete]
     [Route("{id:int}")]
     public async Task<IActionResult> DeleteOrderByIdAsync([FromRoute] int id)
     {
-        return Ok();
+        var result = await orderService.DeleteOrderByIdAsync(id);
+        
+        if (result.Type == ServiceResponseType.Success)
+        {
+            return Ok();
+        }
+        
+        if (result.Type == ServiceResponseType.NotFound)
+        {
+            return NotFound();
+        }
+        
+        return StatusCode(500);
     }
 }
--- SyntriceEShop.Tests/API/Controllers/Implementations/OrderControllerTests.cs
+++ SyntriceEShop.Tests/API/Controllers/Implementations/OrderControllerTests.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Shouldly;
+using SyntriceEShop.API.Controllers.Implementations;
+using SyntriceEShop.API.Models.OrderModel.DTO;
+using SyntriceEShop.API.Services;
+using SyntriceEShop.API.Services.Interfaces;
 using SyntriceEShop.Tests.API.Services.UserServices;
 
 namespace SyntriceEShop.Tests.API.Controllers.Implementations;
@@ -5,28 +12,279 @@ namespace SyntriceEShop.Tests.API.Controllers.Implementations;
 [TestFixture]
 public class OrderControllerTests
 {
+    private OrderController _orderController;
+    private IOrderService _orderService;
+
     [SetUp]
     public void Setup()
     {
+        _orderService = Substitute.For<IOrderService>();
+        _orderController = new OrderController(_orderService);
     }
 
     [TestFixture]
     public class GetAllOrdersAsync : OrderControllerTests
     {
+        [Test]
+        public async Task CallsOrderService_GetAllOrdersAsync()
+        {
+            // Arrange
+            var serviceResult = new ServiceObjectResponse<IEnumerable<GetOrderResponse>>();
+            _orderService.GetAllOrdersAsync().Returns(serviceResult);
+
+            // Act
+            await _orderController.GetAllOrdersAsync();
+
+            // Assert
+            await _orderService.Received(1).GetAllOrdersAsync();
+        }
+
+        [Test]
+        public async Task WhenOrderService_ReturnsEmptyCollection_ReturnsOkObjectResultWithEmptyCollection()
+        {
+            // Arrange
+            var serviceResult = new ServiceObjectResponse<IEnumerable<GetOrderResponse>>()
+            {
+                Type = ServiceResponseType.Success,
+                Value = []
+            };
+            _orderService.GetAllOrdersAsync().Returns(serviceResult);
+
+            // Act
+            var result = await _orderController.GetAllOrdersAsync();
+
+            // Assert
+            result.ShouldBeOfType(typeof(OkObjectResult));
+            var responseValue = (result as OkObjectResult)?.Value as IEnumerable<GetOrderResponse>;
+            responseValue.ShouldBeEmpty();
+        }
+
+        [Test]
+        public async Task WhenOrderService_ReturnsSuccess_ReturnsOkResultWithOrders()
+        {
+            // Arrange
+            var serviceResult = new ServiceObjectResponse<IEnumerable<GetOrderResponse>>()
+            {
+                Type = ServiceResponseType.Success,
+                Value = [new GetOrderResponse(), new GetOrderResponse()]
+            };
+            _orderService.GetAllOrdersAsync().Returns(serviceResult);
+
+            // Act
+            var result = await _orderController.GetAllOrdersAsync();
+
+            // Assert
+            result.ShouldBeOfType(typeof(OkObjectResult));
+            var responseValue = (result as OkObjectResult)?.Value as IEnumerable<GetOrderResponse>;
+            responseValue.ShouldBeEquivalentTo(serviceResult.Value);
+        }
     }
 
     [TestFixture]
     public class GetOrderByIdAsync : OrderControllerTests
     {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(98)]
+        public async Task CallsOrderService_GetOrderByIdAsync(int id)
+        {
+            // Arrange
+            var serviceResult = new ServiceObjectResponse<GetOrderResponse>()
+            {
+                Type = ServiceResponseType.Failure,
+            };
+            _orderService.GetOrderByIdAsync(id).Returns(serviceResult);
+
+
+            // Act
+            var result = await _orderController.GetOrderByIdAsync(id);
+
+            // Assert
+            await _orderService.Received(1).GetOrderByIdAsync(id);
+        }
+
+        [Test]
+        public async Task WhenOrderService_ReturnsSuccess_ReturnsOkObjectResultWithOrder()
+        {
+            // Arrange
+            int id = 1;
+            var serviceResult = new ServiceObjectResponse<GetOrderResponse>()
+            {
+                Type = ServiceResponseType.Success,
+                Value = new GetOrderResponse()
+            };
+            _orderService.GetOrderByIdAsync(id).Returns(serviceResult);
+
+
+            // Act
+            var result = await _orderController.GetOrderByIdAsync(id);
+
+            // Assert
+
+            result.ShouldBeOfType(typeof(OkObjectResult));
+            var responseValue = (result as OkObjectResult)?.Value as GetOrderResponse;
+            responseValue.ShouldBe(serviceResult.Value);
+        }
+
+        [Test]
+        public async Task WhenOrderService_ReturnsNotFound_ReturnsNotFoundResult()
+        {
+            // Arrange
+            int id = 1;
+            var serviceResult = new ServiceObjectResponse<GetOrderResponse>()
+            {
+                Type = ServiceResponseType.NotFound,
+            };
+            _orderService.GetOrderByIdAsync(id).Returns(serviceResult);
+
+
+            // Act
+            var result = await _orderController.GetOrderByIdAsync(id);
+
+            // Assert
+
+            result.ShouldBeOfType(typeof(NotFoundResult));
+        }
     }
+
 
     [TestFixture]
     public class AddOrderAsync : OrderControllerTests
     {
+        [Test]
+        public async Task CallsOrderService_AddOrderAsync()
+        {
+            // Arrange
+            var addOrderRequest = new AddOrderRequest();
+            var serviceResult = new ServiceObjectResponse<int>();
+            _orderService.AddOrderAsync(addOrderRequest).Returns(serviceResult);
+            
+            // Act
+            await _orderController.AddOrderAsync(addOrderRequest);
+            
+            // Assert
+            await _orderService.Received(1).AddOrderAsync(addOrderRequest);
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(99)]
+        public async Task WhenOrderService_ReturnsSuccess_ReturnsOkObjectResultWithId(int id)
+        {
+            // Arrange
+            var addOrderRequest = new AddOrderRequest();
+            var serviceResult = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.Success,
+                Value = id
+            };
+            _orderService.AddOrderAsync(addOrderRequest).Returns(serviceResult);
+            
+            // Act
+            var result = await _orderController.AddOrderAsync(addOrderRequest);
+            
+            // Assert
+            result.ShouldBeOfType<OkObjectResult>();
+            var resutlValue = ((OkObjectResult) result).Value as int?;
+            resutlValue.ShouldBe(id);
+        }
+
+        [Test]
+        public async Task WhenOrderService_ReturnsConflict_ReturnsConflictObjectResultWithMessage()
+        {
+            // Arrange
+            var addOrderRequest = new AddOrderRequest();
+            var serviceResult = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.Conflict,
+                Message = "message"
+            };
+            _orderService.AddOrderAsync(addOrderRequest).Returns(serviceResult);
+            
+            // Act
+            var result = await _orderController.AddOrderAsync(addOrderRequest);
+            
+            // Assert
+            result.ShouldBeOfType<ConflictObjectResult>();
+            var resutlValue = ((ConflictObjectResult) result).Value as string;
+            resutlValue.ShouldBe(serviceResult.Message);
+        }
+
+        [Test]
+        public async Task WhenOrderService_ReturnsValidationError_ReturnsBadRequestResultWithErrors()
+        {
+            // Arrange
+            var addOrderRequest = new AddOrderRequest();
+            var serviceResult = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.ValidationError,
+                Message = "message"
+            };
+            _orderService.AddOrderAsync(addOrderRequest).Returns(serviceResult);
+            
+            // Act
+            var result = await _orderController.AddOrderAsync(addOrderRequest);
+            
+            // Assert
+            result.ShouldBeOfType<BadRequestObjectResult>();
+            var resutlValue = ((BadRequestObjectResult) result).Value as string;
+            resutlValue.ShouldBe(serviceResult.Message);
+        }
     }
 
     [TestFixture]
     public class DeleteOrderByIdAsync : OrderControllerTests
     {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(99)]
+        public async Task CallsOrderService_DeleteOrderByIdAsync(int id)
+        {
+            // Arrange
+            var serviceResult = new ServiceResponse();
+            _orderService.DeleteOrderByIdAsync(id).Returns(serviceResult);
+            
+            // Act
+            var result = await _orderController.DeleteOrderByIdAsync(id);
+            
+            // Assert
+            await _orderService.Received(1).DeleteOrderByIdAsync(id);
+        }
+        
+        [Test]
+        public async Task WhenOrderService_ReturnsSuccess_ReturnsOkResult()
+        {
+            // Arrange
+            int id = 1;
+            var serviceResult = new ServiceResponse()
+            {
+                Type = ServiceResponseType.Success,
+            };
+            _orderService.DeleteOrderByIdAsync(id).Returns(serviceResult);
+            
+            // Act
+            var result = await _orderController.DeleteOrderByIdAsync(id);
+            
+            // Assert
+            result.ShouldBeOfType<OkResult>();
+        }
+        
+        [Test]
+        public async Task WhenOrderService_ReturnsNotFound_ReturnsNotFoundResult()
+        {
+            // Arrange
+            int id = 1;
+            var serviceResult = new ServiceResponse()
+            {
+                Type = ServiceResponseType.NotFound,
+            };
+            _orderService.DeleteOrderByIdAsync(id).Returns(serviceResult);
+            
+            // Act
+            var result = await _orderController.DeleteOrderByIdAsync(id);
+            
+            // Assert
+            result.ShouldBeOfType<NotFoundResult>();
+        }
     }
 }
