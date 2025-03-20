8c8a211b9fbe5cb6e8490ca788b9dd20354b0eb4
Syntrice <syntrice@protonmail.com>
2025-03-20 13:04:49 +0000

    feat: Implement and test ShoppingCartController

--- SyntriceEShop.API/Controllers/Implementations/ShoppingCartController.cs
+++ SyntriceEShop.API/Controllers/Implementations/ShoppingCartController.cs
@@ -1,30 +1,79 @@
 using Microsoft.AspNetCore.Mvc;
 using SyntriceEShop.API.Controllers.Interfaces;
 using SyntriceEShop.API.Models.ShoppingCartModel.DTO;
+using SyntriceEShop.API.Services;
+using SyntriceEShop.API.Services.Interfaces;
 
 namespace SyntriceEShop.API.Controllers.Implementations;
 
 [ApiController]
 [Route("api/shopping-cart")]
-public class ShoppingCartController : ControllerBase, IShoppingCartController
+public class ShoppingCartController(IShoppingCartService shoppingCartService) : ControllerBase, IShoppingCartController
 {
     [HttpPost]
     public async Task<IActionResult> AddShoppingCartAsync([FromBody] AddShoppingCartRequest addShoppingCartRequest)
     {
-        return Ok();
+        var result = await shoppingCartService.AddShoppingCartAsync(addShoppingCartRequest);
+
+        if (result.Type == ServiceResponseType.Success)
+        {
+            return Created();
+        }
+
+        if (result.Type == ServiceResponseType.Conflict)
+        {
+            return Conflict(result.Message);
+        }
+
+        if (result.Type == ServiceResponseType.ValidationError)
+        {
+            return BadRequest(result.Message);
+        }
+        
+        return StatusCode(500);
     }
     
     [HttpDelete]
     [Route("{id:int}")]
     public async Task<IActionResult> DeleteShoppingCartByIdAsync([FromRoute] int id)
     {
-        return Ok();
+        var result = await shoppingCartService.DeleteShoppingCartAsync(id);
+        
+        
+        if (result.Type == ServiceResponseType.Success)
+        {
+            return NoContent();
+        }
+
+        if (result.Type == ServiceResponseType.NotFound)
+        {
+            return NotFound();
+        }
+
+        return StatusCode(500);
     }
     
     [HttpPut]
     [Route("{id:int}")]
     public async Task<IActionResult> UpdateShoppingCartByIdAsync([FromRoute] int id, [FromBody] UpdateShoppingCartRequest updateShoppingCartRequest)
     {
-        return Ok();
+        var result = await shoppingCartService.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest);
+        
+        if (result.Type == ServiceResponseType.Success)
+        {
+            return NoContent();
+        }
+
+        if (result.Type == ServiceResponseType.ValidationError)
+        {
+            return BadRequest(result.Message);
+        }
+        
+        if (result.Type == ServiceResponseType.NotFound)
+        {
+            return NotFound();
+        }
+        
+        return StatusCode(500);
     }
 }
--- SyntriceEShop.Tests/API/Controllers/Implementations/ShoppingCartControllerTests.cs
+++ SyntriceEShop.Tests/API/Controllers/Implementations/ShoppingCartControllerTests.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Shouldly;
+using SyntriceEShop.API.Controllers.Implementations;
+using SyntriceEShop.API.Models.ShoppingCartModel.DTO;
+using SyntriceEShop.API.Services;
+using SyntriceEShop.API.Services.Interfaces;
 using SyntriceEShop.Tests.API.Services.UserServices;
 
 namespace SyntriceEShop.Tests.API.Controllers.Implementations;
@@ -5,23 +12,242 @@ namespace SyntriceEShop.Tests.API.Controllers.Implementations;
 [TestFixture]
 public class ShoppingCartControllerTests
 {
+    private IShoppingCartService _shoppingCartService;
+    private ShoppingCartController _controller;
+    
     [SetUp]
     public void Setup()
     {
+        _shoppingCartService = Substitute.For<IShoppingCartService>();
+        _controller = new ShoppingCartController(_shoppingCartService);
     }
 
     [TestFixture]
     public class AddShoppingCartAsync : ShoppingCartControllerTests
     {
+        [Test]
+        public async Task CallsShoppingCartService_AddShoppingCartAsync()
+        {
+            // Arrange
+            var addShoppingCartRequest = new AddShoppingCartRequest();
+            var addShoppingCartResponse = new ServiceObjectResponse<int>();
+            
+            _shoppingCartService.AddShoppingCartAsync(addShoppingCartRequest).Returns(addShoppingCartResponse);
+            
+            // Act
+            var result = await _controller.AddShoppingCartAsync(addShoppingCartRequest);
+            
+            // Assert
+            await _shoppingCartService.Received(1).AddShoppingCartAsync(addShoppingCartRequest);
+        }
+
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsSuccess_ReturnCreatedResult()
+        {
+            // Arrange
+            int id = 1;
+            var addShoppingCartRequest = new AddShoppingCartRequest();
+            var addShoppingCartResposne = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.Success,
+                Value = id
+            };
+            _shoppingCartService.AddShoppingCartAsync(addShoppingCartRequest).Returns(addShoppingCartResposne);
+            
+            // Act
+            var result = await _controller.AddShoppingCartAsync(addShoppingCartRequest);
+            
+            // Assert
+            result.ShouldBeOfType(typeof(CreatedResult));
+        }
+        
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsConflict_ReturnConflictObjectResultWithMessage()
+        {
+            // Arrange
+            int id = 1;
+            var addShoppingCartRequest = new AddShoppingCartRequest();
+            var addShoppingCartResposne = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.Conflict,
+                Value = id,
+                Message = "conflict result message"
+            };
+            _shoppingCartService.AddShoppingCartAsync(addShoppingCartRequest).Returns(addShoppingCartResposne);
+            
+            // Act
+            var result = await _controller.AddShoppingCartAsync(addShoppingCartRequest);
+            
+            // Assert
+            result.ShouldBeOfType(typeof(ConflictObjectResult));
+            var resultValue = ((ConflictObjectResult)result).Value as string;
+            resultValue.ShouldBe(addShoppingCartResposne.Message);
+        }
+        
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsValidationError_ReturnBadRequestObjectResponse()
+        {
+            // Arrange
+            int id = 1;
+            var addShoppingCartRequest = new AddShoppingCartRequest();
+            var addShoppingCartResposne = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.ValidationError,
+                Value = id,
+                Message = "message"
+            };
+            _shoppingCartService.AddShoppingCartAsync(addShoppingCartRequest).Returns(addShoppingCartResposne);
+            
+            // Act
+            var result = await _controller.AddShoppingCartAsync(addShoppingCartRequest);
+            
+            // Assert
+            result.ShouldBeOfType(typeof(BadRequestObjectResult));
+            var resultValue = ((BadRequestObjectResult)result).Value as string;
+            resultValue.ShouldBe(addShoppingCartResposne.Message);
+        }
     }
 
     [TestFixture]
     public class DeleteShoppingCartAsync : ShoppingCartControllerTests
     {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(99)]
+        public async Task CallsShoppingCartService_DeleteShoppingCartByIdAsync(int id)
+        {
+            // Arrange
+            var deleteShoppingCartResponse = new ServiceObjectResponse<int>();
+            
+            _shoppingCartService.DeleteShoppingCartAsync(id).Returns(deleteShoppingCartResponse);
+            
+            // Act
+            var result = await _controller.DeleteShoppingCartByIdAsync(id);
+            
+            // Assert
+            await _shoppingCartService.Received(1).DeleteShoppingCartAsync(id);
+        }
+        
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsSuccess_ReturnNoContentResult()
+        {
+            // Arrange
+            int id = 1;
+            var deleteShoppingCartResponse = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.Success,
+                Value = id
+            };
+            _shoppingCartService.DeleteShoppingCartAsync(id).Returns(deleteShoppingCartResponse);
+            
+            // Act
+            var result = await _controller.DeleteShoppingCartByIdAsync(id);
+            
+            // Assert
+            result.ShouldBeOfType(typeof(NoContentResult));
+        }
+        
+        
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsNotFound_ReturnNotFoundResult()
+        {
+            // Arrange
+            int id = 1;
+            var deleteShoppingCartResponse = new ServiceObjectResponse<int>()
+            {
+                Type = ServiceResponseType.NotFound,
+                Value = id
+            };
+            _shoppingCartService.DeleteShoppingCartAsync(id).Returns(deleteShoppingCartResponse);
+            
+            // Act
+            var result = await _controller.DeleteShoppingCartByIdAsync(id);
+            
+            // Assert
+            result.ShouldBeOfType(typeof(NotFoundResult));
+        }
     }
 
     [TestFixture]
     public class UpdateShoppingCartByIdAsync : ShoppingCartControllerTests
     {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(99)]
+        public async Task CallsShoppingCartService_UpdateShoppingCartByIdAsync(int id)
+        {
+            // Arrange
+            var updateShoppingCartRequest = new UpdateShoppingCartRequest();
+            var updateShoppingCartResponse = new ServiceResponse();
+            
+            _shoppingCartService.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest).Returns(updateShoppingCartResponse);
+            
+            // Act
+            var result = await _controller.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest);
+            
+            // Assert
+            await _shoppingCartService.Received(1).UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest);
+        }
+        
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsSuccess_ReturnNoContentResult()
+        {
+            // Arrange
+            int id = 1;
+            var updateShoppingCartRequest = new UpdateShoppingCartRequest();
+            var updateShoppingCartResponse = new ServiceResponse()
+            {
+                Type = ServiceResponseType.Success
+            };
+            _shoppingCartService.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest).Returns(updateShoppingCartResponse);
+            
+            // Act
+            var result = await _controller.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest);
+            
+            // Assert
+            result.ShouldBeOfType(typeof(NoContentResult));
+        }
+
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsValidationError_ReturnBadRequestObjectResponseWithMessage()
+        {
+            // Arrange
+            int id = 1;
+            var updateShoppingCartRequest = new UpdateShoppingCartRequest();
+            var updateShoppingCartResponse = new ServiceResponse()
+            {
+                Type = ServiceResponseType.ValidationError,
+                Message = "message"
+            };
+            _shoppingCartService.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest)
+                .Returns(updateShoppingCartResponse);
+
+            // Act
+            var result = await _controller.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest);
+
+            // Assert
+            result.ShouldBeOfType(typeof(BadRequestObjectResult));
+            var resultValue = ((BadRequestObjectResult)result).Value as string;
+            resultValue.ShouldBe(updateShoppingCartResponse.Message);
+        }
+        
+        [Test]
+        public async Task WhenShoppingCartService_ReturnsNotFound_ReturnNotFoundResult()
+        {
+            // Arrange
+            int id = 1;
+            var updateShoppingCartRequest = new UpdateShoppingCartRequest();
+            var updateShoppingCartResponse = new ServiceResponse()
+            {
+                Type = ServiceResponseType.NotFound
+            };
+            _shoppingCartService.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest).Returns(updateShoppingCartResponse);
+            
+            // Act
+            var result = await _controller.UpdateShoppingCartByIdAsync(id, updateShoppingCartRequest);
+            
+            // Assert
+            result.ShouldBeOfType(typeof(NotFoundResult));
+        }
     }
 }
