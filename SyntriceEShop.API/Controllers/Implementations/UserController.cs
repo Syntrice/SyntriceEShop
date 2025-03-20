6c7a3be73651a2cf34a238deeaf26538ecbc0b68
Syntrice <syntrice@protonmail.com>
2025-03-20 13:10:46 +0000

    fix: Fix missing UserController method parameters

--- SyntriceEShop.API/Controllers/Implementations/UserController.cs
+++ SyntriceEShop.API/Controllers/Implementations/UserController.cs
@@ -1,29 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using SyntriceEShop.API.Controllers.Interfaces;
+using SyntriceEShop.API.Services.Interfaces;
 
 namespace SyntriceEShop.API.Controllers.Implementations;
 
 [ApiController]
 [Route("api/user")]
-public class UserController : ControllerBase, IUserController
+public class UserController(IUserService userService) : ControllerBase, IUserController
 {
     [HttpGet]
-    [Route("products")]
-    public async Task<IActionResult> GetAllProductsByUserAsync()
+    [Route("{id:int}/products")]
+    public async Task<IActionResult> GetAllProductsByUserAsync([FromRoute] int userId)
     {
         return Ok();
     }
     
     [HttpGet]
-    [Route("orders")]
-    public async Task<IActionResult> GetAllOrdersByUserAsync()
+    [Route("{id:int}/orders")]
+    public async Task<IActionResult> GetAllOrdersByUserAsync([FromRoute] int userId)
     {
         return Ok();
     }
     
     [HttpGet]
-    [Route("shopping-cart")]
-    public async Task<IActionResult> GetShoppingCartByUserAsync()
+    [Route("{id:int}/shopping-cart")]
+    public async Task<IActionResult> GetShoppingCartByUserAsync([FromRoute] int userId)
     {
         return Ok();
     }
--- SyntriceEShop.API/Controllers/Interfaces/IUserController.cs
+++ SyntriceEShop.API/Controllers/Interfaces/IUserController.cs
@@ -4,7 +4,7 @@ namespace SyntriceEShop.API.Controllers.Interfaces;
 
 public interface IUserController
 {
-    Task<IActionResult> GetAllProductsByUserAsync();
-    Task<IActionResult> GetAllOrdersByUserAsync();
-    Task<IActionResult> GetShoppingCartByUserAsync();
+    Task<IActionResult> GetAllProductsByUserAsync(int userId);
+    Task<IActionResult> GetAllOrdersByUserAsync(int userId);
+    Task<IActionResult> GetShoppingCartByUserAsync(int userId);
 }
