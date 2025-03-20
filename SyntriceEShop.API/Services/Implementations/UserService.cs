d8c42a684cd163218187d789623cdb3361bc25c1
Syntrice <syntrice@protonmail.com>
2025-03-20 13:19:46 +0000

    fix: Update UserService.GetAllOrdersByUserAsync
    Change GetAllOrdersByUserAsync to correct return type

--- SyntriceEShop.API/Services/Implementations/UserService.cs
+++ SyntriceEShop.API/Services/Implementations/UserService.cs
@@ -1,3 +1,4 @@
+using SyntriceEShop.API.Models.OrderModel.DTO;
 using SyntriceEShop.API.Models.ProductModel.DTO;
 using SyntriceEShop.API.Models.ShoppingCartModel.DTO;
 using SyntriceEShop.API.Services.Interfaces;
@@ -11,9 +12,9 @@ public class UserService : IUserService
         return new ServiceObjectResponse<IEnumerable<GetProductResponse>>();
     }
 
-    public async Task<ServiceObjectResponse<GetProductResponse>> GetAllOrdersByUserAsync(int userId)
+    public async Task<ServiceObjectResponse<IEnumerable<GetOrderResponse>>> GetAllOrdersByUserAsync(int userId)
     {
-        return new ServiceObjectResponse<GetProductResponse>();
+        return new ServiceObjectResponse<IEnumerable<GetOrderResponse>>();
     }
 
     public async Task<ServiceObjectResponse<GetShoppingCartResponse>> GetShoppingCartByUserAsync(int userId)
--- SyntriceEShop.API/Services/Interfaces/IUserService.cs
+++ SyntriceEShop.API/Services/Interfaces/IUserService.cs
@@ -1,3 +1,4 @@
+using SyntriceEShop.API.Models.OrderModel.DTO;
 using SyntriceEShop.API.Models.ProductModel.DTO;
 using SyntriceEShop.API.Models.ShoppingCartModel.DTO;
 
@@ -6,6 +7,6 @@ namespace SyntriceEShop.API.Services.Interfaces;
 public interface IUserService
 {
     Task<ServiceObjectResponse<IEnumerable<GetProductResponse>>> GetAllProductsByUserAsync(int userId);
-    Task<ServiceObjectResponse<GetProductResponse>> GetAllOrdersByUserAsync(int userId);
+    Task<ServiceObjectResponse<IEnumerable<GetOrderResponse>>> GetAllOrdersByUserAsync(int userId);
     Task<ServiceObjectResponse<GetShoppingCartResponse>> GetShoppingCartByUserAsync(int userId);
 }
