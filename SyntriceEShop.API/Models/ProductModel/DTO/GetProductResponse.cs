a1002bc38b42c1d66353be00d4c237cf91d27a03
Syntrice <syntrice@protonmail.com>
2025-03-21 12:43:50 +0000

    feat: Implement ProductDTOs

--- SyntriceEShop.API/Models/ProductModel/DTO/GetProductResponse.cs
+++ SyntriceEShop.API/Models/ProductModel/DTO/GetProductResponse.cs
@@ -2,5 +2,11 @@ namespace SyntriceEShop.API.Models.ProductModel.DTO;
 
 public class GetProductResponse
 {
-    
+    public int Id { get; set; }
+    public int ProductCategoryId { get; set; }
+    public int UserId { get; set; }
+    public decimal Price { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int QuantityInStock { get; set; }
 }
