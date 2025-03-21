1b5c4cf781d2f70e349dcbd5974875083df5332b
Syntrice <syntrice@protonmail.com>
2025-03-21 13:09:45 +0000

    feat: Implement and test ProductMapper

--- SyntriceEShop.API/Models/ProductModel/ProductMapper.cs
+++ SyntriceEShop.API/Models/ProductModel/ProductMapper.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Server.HttpSys;
 using SyntriceEShop.API.Models.ProductModel.DTO;
 
 namespace SyntriceEShop.API.Models.ProductModel;
@@ -6,16 +7,41 @@ public static class ProductMapper
 {
     public static Product ToProduct(this AddProductRequest request)
     {
-        return new Product();
+        return new Product
+        {
+            Description = request.Description,
+            ProductCategoryId = request.ProductCategoryId,
+            Name = request.Name,
+            Price = request.Price,
+            UserId = request.UserId,
+            QuantityInStock = request.QuantityInStock,
+        };
     }
 
     public static Product ToProduct(this UpdateProductRequest request)
     {
-        return new Product();
+        return new Product
+        {
+            Description = request.Description,
+            ProductCategoryId = request.ProductCategoryId,
+            Name = request.Name,
+            Price = request.Price,
+            UserId = request.UserId,
+            QuantityInStock = request.QuantityInStock,
+        };
     }
 
     public static GetProductResponse ToGetProductResponse(this Product product)
     {
-        return new GetProductResponse();
+        return new GetProductResponse
+        {
+            Id = product.Id,
+            Description = product.Description,
+            ProductCategoryId = product.ProductCategoryId,
+            Name = product.Name,
+            Price = product.Price,
+            UserId = product.UserId,
+            QuantityInStock = product.QuantityInStock,
+        };
     }
 }
--- SyntriceEShop.Tests/API/Models/ProductModel/ProductMapperTests.cs
+++ SyntriceEShop.Tests/API/Models/ProductModel/ProductMapperTests.cs
@@ -1,3 +1,8 @@
+using System.Security.Cryptography.X509Certificates;
+using Shouldly;
+using SyntriceEShop.API.Models.ProductModel;
+using SyntriceEShop.API.Models.ProductModel.DTO;
+
 namespace SyntriceEShop.Tests.API.Models.ProductModel;
 
 [TestFixture]
@@ -6,15 +11,91 @@ public class ProductMapperTests
     [TestFixture]
     public class AddProductRequestToProduct : ProductMapperTests
     {
+        [Test]
+        public void MapsDirectMappingsCorrectly()
+        {
+            // Arrange
+            var input = new AddProductRequest()
+            {
+                Name = "name",
+                Description = "this is a description",
+                Price = 98_00,
+                ProductCategoryId = 12,
+                UserId = 56,
+                QuantityInStock = 999
+            };
+            
+            // Act
+            var result = input.ToProduct();
+            
+            // Assert
+            result.Name.ShouldBe(input.Name);
+            result.Description.ShouldBe(input.Description);
+            result.Price.ShouldBe(input.Price);
+            result.ProductCategoryId.ShouldBe(input.ProductCategoryId);
+            result.UserId.ShouldBe(input.UserId);
+            result.QuantityInStock.ShouldBe(input.QuantityInStock);
+        }
     }
 
     [TestFixture]
     public class UpdateProductRequestToProduct : ProductMapperTests
     {
+        [Test]
+        public void MapsDirectMappingsCorrectly()
+        {
+            // Arrange
+            var input = new UpdateProductRequest()
+            {
+                Name = "name",
+                Description = "this is a description",
+                Price = 98_00,
+                ProductCategoryId = 12,
+                UserId = 56,
+                QuantityInStock = 999
+            };
+            
+            // Act
+            var result = input.ToProduct();
+            
+            // Assert
+            result.Name.ShouldBe(input.Name);
+            result.Description.ShouldBe(input.Description);
+            result.Price.ShouldBe(input.Price);
+            result.ProductCategoryId.ShouldBe(input.ProductCategoryId);
+            result.UserId.ShouldBe(input.UserId);
+            result.QuantityInStock.ShouldBe(input.QuantityInStock);
+        }
     }
 
     [TestFixture]
     public class ProductToGetProductResponse : ProductMapperTests
     {
+        [Test]
+        public void MapsDirectMappingsCorrectly()
+        {
+            // Arrange
+            var input = new Product()
+            {
+                Id = 85,
+                Name = "name",
+                Description = "this is a description",
+                Price = 98_00,
+                ProductCategoryId = 12,
+                UserId = 56,
+                QuantityInStock = 999
+            };
+            
+            // Act
+            var result = input.ToGetProductResponse();
+            
+            // Assert
+            result.Name.ShouldBe(input.Name);
+            result.Description.ShouldBe(input.Description);
+            result.Price.ShouldBe(input.Price);
+            result.ProductCategoryId.ShouldBe(input.ProductCategoryId);
+            result.UserId.ShouldBe(input.UserId);
+            result.QuantityInStock.ShouldBe(input.QuantityInStock);
+        }
     }
 }
